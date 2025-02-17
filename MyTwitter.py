cef7c89c2614f8cbadaf03c47ec1c4b63081cd29
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2025-02-17 12:44:19 +0000

    parte 1 - ok

--- MyTwitter.py
+++ MyTwitter.py
@@ -1,10 +1,6 @@
 
-#Precisamos utilizar o método "atualizar" para atualiza as informações do perfil
-#Classes que faltam finalizar:
-    # - seguidores, seguidos, numero de seguidores, tweetar
-#Todas as classes precisam de ajustes e revisões 
-#Precisamos adicionar os argumentos exigidos nas exceções
-
+#Precisamos utilizar o método "atualizar" para atualiza as informações do perfil?
+ 
 class MyTwitter:
     def __init__(self):
         self.__repositorio = RepositorioUsuarios()
@@ -13,37 +9,38 @@ class MyTwitter:
         perfil = self.__repositorio.buscar(usuario)
 
         if perfil is not None:
-            raise PEException()
+            raise PEException(usuario)
         self.__repositorio.cadastrar(perfil)
 
     def cancelar_perfil(self, usuario):
         perfil = self.__repositorio.buscar(usuario)
 
         if perfil is None:
-            raise PIException()
+            raise PIException(usuario)
         elif perfil is not None and perfil.is_ativo() == False:
-            raise PDException()
+            raise PDException(usuario)
         else:
             perfil.set_ativo(False)
 
+
     def tweetar(self, usuario, mensagem):
         perfil = self.__repositorio.buscar(usuario)
-
+        
         if perfil is None or perfil.is_ativo() == False:
-            raise PIException()
+            raise PIException(usuario)
         elif len(mensagem) < 1 or len(mensagem) > 140:
-            raise MFPException() 
+            raise MFPException(usuario) 
         else:
-            #Tem alguma coisa faltando aqui e errada
-            perfil.add_tweet(mensagem)
+            tweet = Tweet(usuario, mensagem)
+            perfil.add_tweet(tweet)
 
     def timeline(self, usuario):
         perfil = self.__repositorio.buscar(usuario)
 
         if perfil is None: 
-            raise PIException()
+            raise PIException(usuario)
         elif perfil is not None and perfil.is_ativo() == False:
-            raise PDException()
+            raise PDException(usuario)
         else: 
             perfil.get_timeline()
 
@@ -51,9 +48,9 @@ class MyTwitter:
         perfil = self.__repositorio.buscar(usuario)
 
         if perfil is None:
-            raise PIException()
+            raise PIException(usuario)
         elif perfil is not None and perfil.is_ativo() == False:
-            raise PDException()
+            raise PDException(usuario)
         else:
             perfil.get_tweets()
 
@@ -62,9 +59,9 @@ class MyTwitter:
         perfil_seguidor = self.__repositorio.buscar(usuario_seguidor)
 
         if perfil_seguido is None or perfil_seguidor is None:
-            raise PIException()
+            raise PIException(usuario)
         elif perfil_seguido.is_ativo() == False or perfil_seguidor.is_ativo() == False:
-            raise PDException()
+            raise PDException(usuario)
         else:
             perfil_seguido.add_seguidor(perfil_seguidor)
             perfil_seguidor.add_seguidos(perfil_seguido)
@@ -74,34 +71,44 @@ class MyTwitter:
         perfil = self.__repositorio.buscar(usuario)
 
         if perfil is None:
-            raise PIException()
+            raise PIException(usuario)
         elif perfil is not None and perfil.is_ativo() == False:
-            raise PDException()
+            raise PDException(usuario)
         else:
-            #Precisamos de:
-                # - um for para verificar por seguidor se ele existe e é ativo
-                # - uma variável count para contabilizar o n° de seguidores 
-
+            count = 0
+            for seguidor in perfil.seguidores():
+                seguidor = self.__repositorio.buscar(seguidor)
+                if seguidor is not None and seguidor.is_ativo():
+                    count += 1
+            return count
+    
     def seguidores(self):
-        perfil = self.__repositorio.buscar()
+        perfil = self.__repositorio.buscar(usuario)
 
         if perfil is None: 
-            raise PIException()
+            raise PIException(usuario)
         elif perfil is not None and perfil.is_ativo() == False:
-            raise PDException()
+            raise PDException(usuario)
         else:
-            #Aqui falta levar em consideração somente os perfis_seguidores
-            #que estiverem ativos
-            perfil.__seguidores()
+            list_seguidores = []
+            for seguidor in perfil.seguidores():
+                seguidor = self.__repositorio.buscar(seguidor)
+                if seguidor is not None and seguidor.is_ativo() == True:
+                    list_seguidores.append(seguidor)
+            return list_seguidores
+
 
     def seguidos(self):
-        perfil = self.__repositorio.buscar()
+        perfil = self.__repositorio.buscar(usuario)
         
         if perfil is None: 
-            raise PIException()
+            raise PIException(usuario)
         elif perfil is not None and perfil.is_ativo() == False:
-            raise PDException()
+            raise PDException(usuario)
         else:
-            #Aqui falta levar em consideração somente os perfis_seguidores
-            #que estiverem ativos
-            perfil.__seguidos()
+            list_seguidos = []
+            for seguido in perfil.seguidos():
+                seguido = self.__repositorio.buscar(seguido)
+                if seguido is not None and seguido.is_ativo() == True:
+                    list_seguido.append(seguido)
+            return list_seguido
